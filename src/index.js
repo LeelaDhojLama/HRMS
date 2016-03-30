a9325e8f2c1d919644e471bf467d1479dbaab934
kmsheng <kmsh3ng@gmail.com>
2016-03-30 18:35:07 +0800

    add index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('babel-polyfill');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './containers/Main';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
