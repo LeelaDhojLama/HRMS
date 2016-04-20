cf3f3ea181ce89e3d33cd27f7906fdda852c1c25
zhi-xuan <rickie120243@gmail.com>
2016-04-20 15:35:56 +0800

    trigger asideButton

--- src/containers/asideButton.js
+++ src/containers/asideButton.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import store from '../store/index.js';
 
-export default class AsideButton extends Component {
-  render() {
+const renderAsideButton = (state) => {
+  if(state.signState.sign) {
     return (
       <div id="asidebutton">
-      <p><button id="clockInOut" className="asidebtn">簽到/簽退</button></p>
-      <p><button id="clockInOut" className="asidebtn">請假</button></p>
-      <p><button id="clockInOut" className="asidebtn">加班</button></p>
-      <p><button id="clockInOut" className="asidebtn">統計報表</button></p>
+      <p><button id="clockInOutbtn" className="asidebtn">簽到/簽退</button></p>
+      <p><button id="leavebtn" className="asidebtn">請假</button></p>
+      <p><button id="overTimebtn" className="asidebtn">加班</button></p>
+      <p><button id="reportbtn" className="asidebtn">統計報表</button></p>
       </div>
     );
+  } else {
+    return (
+      <div id="asidebutton"></div>
+    );
   }
-
 }
+
+const mapStateToProps = (state) => {
+  return {
+    signState: state
+  }
+}
+
+const AsideButton = connect(mapStateToProps)(renderAsideButton)
+
+export default AsideButton
